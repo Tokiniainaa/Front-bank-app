00d0e0a8ef26b4c5ff0919b3c7e9cc1ebf776cf6
tokiniaina <hei.tokiniaina@gmail.com>
2024-03-24 00:28:17 +0300

    feat: add Dashboard

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Dashboard.css";
 import Navigation from "../Components/navigation.tsx";
+import MyChart from "./chart.tsx";
 
 const Dashboard = () => {
   return (
@@ -18,9 +19,20 @@ const Dashboard = () => {
              </select>
              </div>
           <div className="bigDash">
-            <div className="dashboard"></div>
-            <div className="dashboard1"></div>
-            <div className="dashboard2"></div>
+            <div className="dashboard">
+              <h2>Dashboard</h2>
+              <MyChart ></MyChart >
+            </div>
+            <div className="dashboard1">
+              <h2>Sold Evolution</h2>
+              <div>
+                <p className="DAY">TODAY</p>
+                <p className="devise">0 MGA</p>
+              </div>
+            </div>
+            <div className="dashboard2">
+              <h2>Last transaction</h2>
+            </div>
            
           </div>
         </div>
---
+++ src/Pages/chart.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import './chart.css';
+import 'chart.js/auto';
+
+const data = {
+    labels: ["Jan","Feb","Mar","Apr","May"],
+    datasets: [
+        {
+            label: 'Solde',
+            data: [1,5,10,2,9,2,3,4,5,12],
+            fill: false,
+            backgroundColor: '#0071bd', 
+            borderColor: '#0071bd',
+        },
+        {
+            label: 'Balance',
+            data: [1,1,2,4,5,6,7,8,6,7,8,2,1,3],
+            fill: false,
+            backgroundColor: 'green', 
+            borderColor: 'green',
+        },
+        {
+            label: 'Spending',
+            data: [1,2,3,4,5,5,6,6,,7,8,8,9,10],
+            fill: false,
+            backgroundColor: 'red', 
+            borderColor: 'red',
+        }
+    ],
+};
+
+const MyChart = () => (
+    <div className="Data"> 
+        <Line className='own-agenda' data={data} />
+    </div>
+);
+
+export default MyChart;
