85f4162557ebb00c42c00b0e5ffb57c766a5f79b
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-27 18:09:45 +0300

    feat: map card for transaction and showing addButton component

--- src/Components/Transaction/Transaction.jsx
+++ src/Components/Transaction/Transaction.jsx
@@ -1,8 +1,19 @@
+import { Box, Text } from "@chakra-ui/react"
+import TransactionsData from '../../Transactionmock.json'
+import TransactionDetailsModal from "./TransactionDetailsModal/TransactionDetailsModal"
+import AddButton from "../AddButton/AddButton"
+
 const Transaction = () => {
     return(
-        <div>
-            Transaction
-        </div>
+        <Box>
+            <Text m={4}>Transactions</Text>
+            <AddButton name={"Make new transaction"} title={"Make transaction"}></AddButton>
+            <Box>
+                {TransactionsData.map((transaction, index) => (
+                    <TransactionDetailsModal key={index} transaction={transaction} />
+                ))}
+            </Box>
+        </Box>
     )
 }
 export default Transaction
