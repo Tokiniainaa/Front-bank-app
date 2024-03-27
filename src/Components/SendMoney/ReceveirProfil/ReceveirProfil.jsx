f3f0c519705fe8916d13665104182ecbf0cb6aa5
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-27 21:40:24 +0300

    feat: create receiver's profil component

---
+++ src/Components/SendMoney/ReceveirProfil/ReceveirProfil.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react"
+
+const ReceveirProfil = () => {
+    return(
+        <Box w="35%" h="100%" p={4}>
+
+        </Box>
+    )
+}
+export default ReceveirProfil;
