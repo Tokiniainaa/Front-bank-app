0147ac077f5f8c46403e0649a6d6cdd9b14b3778
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-27 21:41:12 +0300

    feat: add receiver container and profil

--- src/Components/SendMoney/SendMoney.jsx
+++ src/Components/SendMoney/SendMoney.jsx
@@ -1,8 +1,13 @@
+import { Flex } from "@chakra-ui/react"
+import ReceiverContainer from "./ReceiverContainer/ReceiverContainer"
+import ReceveirProfil from "./ReceveirProfil/ReceveirProfil"
+
 const SendMoney = () => {
     return(
-        <div>
-            Send Money
-        </div>
+        <Flex direction="row" h="100vh">
+            <ReceiverContainer></ReceiverContainer>
+            <ReceveirProfil></ReceveirProfil>
+        </Flex>
     )
 }
 export default SendMoney
