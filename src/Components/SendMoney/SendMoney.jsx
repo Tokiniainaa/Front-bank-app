425294320d020a984e5655d0e45effc7e7a6c697
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-28 19:07:28 +0300

    feat: add handleReceiver method

--- src/Components/SendMoney/SendMoney.jsx
+++ src/Components/SendMoney/SendMoney.jsx
@@ -1,12 +1,19 @@
 import { Flex } from "@chakra-ui/react"
 import ReceiverContainer from "./ReceiverContainer/ReceiverContainer"
 import ReceveirProfil from "./ReceveirProfil/ReceveirProfil"
+import { useState } from "react"
 
 const SendMoney = () => {
+    const [receiver, setReceiver] = useState(null);
+
+    const handleReceiverMethod = (receiver) => {
+        setReceiver(receiver)
+    }
+
     return(
         <Flex direction="row" h="100vh">
-            <ReceiverContainer></ReceiverContainer>
-            <ReceveirProfil></ReceveirProfil>
+            <ReceiverContainer handleReceiverMethod={handleReceiverMethod}></ReceiverContainer>
+            <ReceveirProfil receiver={receiver}></ReceveirProfil>
         </Flex>
     )
 }
