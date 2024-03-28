aa5fba8d8083bb24f5caf804c0b8ce31ba871f4c
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-28 19:03:18 +0300

    feat: add profil directory for receiver

---
+++ src/Components/SendMoney/ReceveirProfil/Profil/ProfilDefault.jsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react"
+
+const ProfilDefault = () => {
+    return(
+        <Box p={5}>
+            <Text>
+                You can choose which receiver will you send money
+            </Text>
+        </Box>
+    )
+}
+
+export default ProfilDefault;
