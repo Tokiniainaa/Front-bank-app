3dadc9967f3c09344a9ade7894207a7cc7bf2224
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-28 19:06:46 +0300

    feat: add profil and profil default component

--- src/Components/SendMoney/ReceveirProfil/ReceveirProfil.jsx
+++ src/Components/SendMoney/ReceveirProfil/ReceveirProfil.jsx
@@ -1,9 +1,14 @@
 import { Box } from "@chakra-ui/react"
+import ProfilDefault from "./Profil/ProfilDefault"
+import Profil from "./Profil/Profil"
 
-const ReceveirProfil = () => {
+const ReceveirProfil = ({ receiver }) => {
+    
     return(
         <Box w="35%" h="100%" p={4}>
-
+            {receiver ? (
+                <Profil receiver={receiver}/>
+            ) : <ProfilDefault />}
         </Box>
     )
 }
