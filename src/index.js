4c4f9b3a9e807f7e9a9409403c97bc19df36fa02
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-28 19:07:59 +0300

    feat: add a theme provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App'
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider , extendTheme } from '@chakra-ui/react';
 
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px)",
+  color: "gray.500"
+} 
+
+export const theme = extendTheme({
+  components: {
+      Form: {
+          variants: {
+              floating: {
+                  container: {
+                      _focusWithin: {
+                          label: {
+                              ...activeLabelStyles
+                          }
+                      },
+                      "input:not(:placeholder-shown) + label, .chakra-select_wrapper + label, textarea:not(:placeholder-shown) ~ label": {
+                          ...activeLabelStyles
+                      },
+                      label: {
+                          top: 0,
+                          left: 0,
+                          zIndex: 2,
+                          position: "absolute",
+                          backgroundColor: "transparent",
+                          pointerEvents: "none",
+                          mx: 3,
+                          px: 1,
+                          my: 2,
+                          transformOrigin: "left top"
+                      }
+                  }
+              }
+          }
+      }
+  }
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
