68a2426dca984e8e139abf479385cf2a9df1520d
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-28 19:05:07 +0300

    feat: get total balances of all accounts

--- src/Components/SendMoney/ReceiverContainer/ReceiverContainer.jsx
+++ src/Components/SendMoney/ReceiverContainer/ReceiverContainer.jsx
@@ -1,18 +1,23 @@
 import { Box, Text } from "@chakra-ui/react"
 import ReceiverList from "../ReceiverList/ReceiverList"
 import AccountMock from "../../../AccountMock.json"
+import Mock from "../../../mock.json"
 
-const ReceiverContainer = () => {
+const ReceiverContainer = ({ handleReceiverMethod }) => {
+    const balances = Mock.map(item => parseInt(item.balance));
+
+    const totalBalance = balances.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
     return(
         <Box w="65%" h="100%" borderRight="1px solid" borderColor="#dbdbdb" p={6}>
             <Box w="100%" borderRadius={7} bg="currentcolor" p={7}>
                 <Text fontSize="xl" color="#fff">
-                    300000 MGA
+                    {totalBalance} MGA
                 </Text>
                 <Text fontSize="sm" color="gray.500">Available balance</Text>
             </Box>
             <Text fontWeight = "bold" paddingTop={4}>Choose receiver</Text>
-            <ReceiverList accounts={AccountMock}></ReceiverList>
+            <ReceiverList accounts={AccountMock} handleReceiver={handleReceiverMethod}></ReceiverList>
         </Box>
     )
 }
