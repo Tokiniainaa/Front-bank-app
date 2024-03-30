d312ddf9799eb97061663d61ed738d298576538d
tokiniaina <hei.tokiniaina@gmail.com>
2024-03-30 10:57:03 +0300

    feat: using chakra for the style

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,5 +1,5 @@
 import react from "react";
-import { Icon } from "@chakra-ui/react";
+import { Icon,Box,Text,Flex,Input,Button } from "@chakra-ui/react";
 import { BellIcon, EmailIcon } from "@chakra-ui/icons";
 import { FaUser } from "react-icons/fa";
 import { RiSunLine } from "react-icons/ri";
@@ -7,43 +7,44 @@ import "./Settings.css";
 
 
 
+
 const Settings = () => {
   return (
-    <div className="all">
-      <h1 className="settigns">Settings</h1>
-      <p className="can">You can change your information here</p>
-      <div className="changeInfo">
-        <div className="left">
-          <div><Icon as={FaUser} w={3.5} h={3.5} cursor="pointer" color="#090d1e" marginRight="10px" />Account</div>
-          <div><Icon as={BellIcon}   w={4} h={4} cursor="pointer" color="#090d1e" marginRight="10px" /> Notification</div>
-          <div><Icon as={RiSunLine} w={4} h={4} cursor="pointer" color="#090d1e" marginRight="10px" />Help</div>
+    <Box bg="rgb(244, 243, 243)" paddingLeft="5%"  paddingTop="2%" w="100%" h="100%">
+      <Text fontSize={"x-large"} fontWeight="bolder" marginBottom="3px">Settings</Text>
+      <Text fontSize={"smaller"} marginBottom={"30px"}  color={"rgba(0, 0, 0, 0.439)"} >You can change your information here</Text>
+      <Flex flexDirection={"row"} w={"80%"}  h={"50%"} justifyContent={"space-between"}>
+        <Box bg={"white"} w={"30%"} paddingTop={"3%"} paddingLeft={"2%"}>
+          <Flex fontSize={"14px"} fontWeight={"700"} color={" rgba(0, 0, 0, 0.84)"} marginBottom={"10px"} flexDirection={"row"} alignItems={"center"} w={"40%"} ><Icon as={FaUser} w={3.5} h={3.5} cursor="pointer" color="#090d1e" marginRight="10px" />Account</Flex >
+          <Flex fontSize={"14px"} fontWeight={"700"} color={" rgba(0, 0, 0, 0.84)"} marginBottom={"10px"} flexDirection={"row"} alignItems={"center"} w={"40%"}><Icon as={BellIcon}   w={4} h={4} cursor="pointer" color="#090d1e" marginRight="10px" /> Notification</Flex>
+          <Flex fontSize={"14px"} fontWeight={"700"} color={" rgba(0, 0, 0, 0.84)"} marginBottom={"10px"} flexDirection={"row"} alignItems={"center"} w={"40%"}><Icon as={RiSunLine} w={4} h={4} cursor="pointer" color="#090d1e" marginRight="10px" />Help</Flex>
           
-        </div>
-        <div className="right">
-          <h2>General Info</h2>
-          <div className="info">
+        </Box>
+        <Box bg={"white"} w={"69.7%"} paddingTop={"2%"}  paddingLeft={"2%"}>
+          <Text fontSize={"16px"} fontWeight={"700"} marginTop={"4px"} marginBottom={"20px"}> General Info</Text>
+          <Box display={"grid"} gridTemplateColumns={"auto auto"}  gridTemplateRows={"auto auto"} gridGap={"10px"} w={"97%"}>
             <div className="inform">
-              <h4>FIRST NAME</h4>
+              <Text fontSize={"x-small"} fontWeight={"600"} marginBottom={"6px"} marginTop={"5px"} color={"rgba(0, 0, 0, 0.777)"}>FIRST NAME</Text >
+              <Input w={"100%"} borderRadius={"2px"}  h={"70%"} outline={"none"} paddingLeft={"10px"} border={"2px solid rgba(0, 0, 0, 0.425)"}/>
+            </div>
+            <div className="inform">
+              <Text fontSize={"x-small"} fontWeight={"600"} marginBottom={"6px"} marginTop={"5px"} color={"rgba(0, 0, 0, 0.777)"} >LAST NAME</Text >
               <input type="text" name="" id="" />
             </div>
             <div className="inform">
-              <h4>LAST NAME</h4>
-              <input type="text" name="" id="" />
-            </div>
-            <div className="inform">
-              <h4>BIRTHDATE</h4>
+              <Text fontSize={"x-small"} fontWeight={"600"} marginBottom={"6px"} marginTop={"5px"} color={"rgba(0, 0, 0, 0.777)"} >BIRTHDATE</Text >
               <input type="date" name="" id="" />
             </div>
             <div className="inform">
-              <h4>SALARY</h4>
+              <Text fontSize={"x-small"} fontWeight={"600"} marginBottom={"6px"} marginTop={"5px"} color={"rgba(0, 0, 0, 0.777)"} >SALARY</Text >
               <input type="text" name="" id="" />
             </div>
-          </div>
+          </Box>
         
-        <button type="button" className="btn">Change</button>
-        </div>
-      </div>
-    </div>
+        <Button marginTop={"10%"} border={"1px solid black"} w={"20%"} h={"12%"} bg={"black"} color={"white"} borderRadius={"20px"} marginLeft={"2px"}>Change</Button>
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 export default Settings;
