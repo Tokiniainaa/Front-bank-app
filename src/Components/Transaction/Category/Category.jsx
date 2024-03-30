608dc03e493bf1bfc6ebf2a5d8e8291eeb3b4bc2
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-30 23:44:59 +0300

    feat: create category list

---
+++ src/Components/Transaction/Category/Category.jsx
@@ -0,0 +1,15 @@
+const Category = [
+    'Food_and_Drinks',
+    'Online_Shopping',
+    'Housing',
+    'Transportation',
+    'Vehicle',
+    'Leisure',
+    'Multimedia_and_Computers',
+    'Financial_Expenses',
+    'Investments',
+    'Income',
+    'Other',
+    'Unknown'
+]
+export default Category
