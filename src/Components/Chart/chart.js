427a74c2c5ca70fd0319c1e75e12eda58e9fcd5b
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-31 23:07:10 +0300

    feat: rearrrange component

--- src/Components/Chart/chart.js
+++ src/Components/Chart/chart.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
-import { Box, Text,Flex ,Input,Button} from "@chakra-ui/react";
+import { Text, Box } from "@chakra-ui/react";
 import 'chart.js/auto';
 
-
-const data = {
-    labels: [
-        'Solde',
-        'Amount',
-        'Category'
-      ],
-      datasets: [{
-        data: [100, 100,100],
-        backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-};
-
-const MyChart = () => (
-    <Flex flexDirection={"column"} paddingLeft={"5%"} w={"60%"} h={"70%"} margin={"0 auto"}> 
-        <Text fontSize={"larger"} fontWeight={"700"} marginBottom={"20px"}>Growth Analyse</Text> 
-        <Pie  data={data}  />
-    </Flex>
+const Chart = ({ datas }) => (
+    <Box marginInline="auto" w="fit-content"> 
+        <Text textAlign="center" >Growth Analyse </Text> 
+        <Box mx="auto">
+            <Pie  data={datas}  />
+        </Box>
+    </Box>
 );
 
-export default MyChart;
+export default Chart;
