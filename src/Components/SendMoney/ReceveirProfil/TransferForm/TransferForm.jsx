e686069b71575092bdec9302985c4e2d925f90a9
Tombosoa <hei.nantenaina.4@gmail.com>
2024-04-01 23:45:35 +0300

    feat: add method to post transfer

--- src/Components/SendMoney/ReceveirProfil/TransferForm/TransferForm.jsx
+++ src/Components/SendMoney/ReceveirProfil/TransferForm/TransferForm.jsx
@@ -1,29 +1,96 @@
 import React, { useEffect, useState } from "react";
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack, Text, Select } from "@chakra-ui/react";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import clientData from "../../../../clientInfo.json"
 
-
-const TransferForm = ({ idAccountCredited, idAccountDebited }) => {
-    const { register, handleSubmit, setValue } = useForm();
+const TransferForm = ({ idAccountCredited }) => {
+    const baseUrl = process.env.REACT_APP_BASE_URL;
+    const clientId = clientData.id_client;
     const [showDateInput, setShowDateInput] = useState(false);
+    const [isScheduled, setIsScheduled] = useState(false);
+    const [scheduledDate, setScheduledDate] = useState('');
+    const [allAccount, setAllAccount] = useState()
+    const { register, handleSubmit, control, reset, getValues, setValue } = useForm({
+      defaultValues: {
+        idAccountDebited: '',
+      }
+    });
+
+    useEffect(()=>{
+      const fetchData = async () => {
+          const response = await axios.get(`${baseUrl}/account/${clientId}`);
+          if (response && response.data && response.data.length > 0){
+              setAllAccount(response.data)
+              reset({
+                ...getValues(),
+                idAccountDebited: response.data[0].id,
+              });
+          }
+      }
+      fetchData()
+    }, [baseUrl, clientId, reset, getValues]) 
 
     useEffect(()=>{
         setValue("idAccountCredited", idAccountCredited);
-        setValue("idAccountDebited", idAccountDebited);
-    }, [setValue,idAccountCredited,idAccountDebited])
-
-
+    }, [setValue,idAccountCredited])
     const onSubmit = (data) => {
-        console.log(data);
+        try{
+            let url = `${baseUrl}/transfer?amount=${data.amount}&idAccountDebited=${data.idAccountDebited}&idAccountCredited=${data.idAccountCredited}&label=${data.label}`;
+            if (isScheduled) {
+                const isoDateString = new Date(scheduledDate).toISOString();
+                const localDate = isoDateString.split('T')[0]; 
+                url = `${baseUrl}/scheduledTransfer?amount=${data.amount}&idAccountDebited=${data.idAccountDebited}&idAccountCredited=${data.idAccountCredited}&label=${data.label}&newEffectiveDate=${localDate}`;
+            }
+            const response = axios.post(url)
+            console.log(response)
+        }catch(error){
+            console.log(error);
+        }
     };
+    
+    
 
     const handleScheduledClick = () => {
         setShowDateInput(true);
+        setIsScheduled(true); 
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <VStack spacing={4} align="start">
+            <Text>Choose your account: </Text>
+                <FormControl variant="floating" id="idAccountDebited" marginBottom={1}>
+                    <Controller
+                        name="idAccountDebited"
+                        control={control}
+                        defaultValue={''}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                border="0"
+                                borderBottom="2px"
+                                borderBottomColor="blue.700"
+                                sx={{
+                                    ':focus': {
+                                        outline: 'none',
+                                        boxShadow: 'none',
+                                    },
+                                }}
+                            >
+                                {allAccount && (
+                                    <>
+                                        {allAccount.map((account) => (
+                                        <option key={account.id} value={account.id}>
+                                            {account.id} ({account.name})
+                                        </option>
+                                ))}
+                                    </>
+                                )}
+                            </Select>
+                        )}
+                    />
+                </FormControl>
             <FormControl variant="floating" id="amount" marginBottom={3}>
                 <InputGroup>
                     <InputRightElement pointerEvents="none"
@@ -71,6 +138,7 @@ const TransferForm = ({ idAccountCredited, idAccountDebited }) => {
                     <FormControl id="date">
                         <FormLabel>Date</FormLabel>
                         <Input {...register("date")} type="date" 
+                          onChange={(e) => setScheduledDate(e.target.value)}
                           outline="none"
                           border="none"
                           borderBottom="2px" 
@@ -78,8 +146,8 @@ const TransferForm = ({ idAccountCredited, idAccountDebited }) => {
                           boxShadow="none"
                           sx={{
                               ':focus': {
-                                  outline: 'none',
-                                  boxShadow: 'none'
+                                 outline: 'none',
+                                 boxShadow: 'none'
                               },
                           }}
                         />
